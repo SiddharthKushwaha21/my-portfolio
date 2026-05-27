80bd481a30bf98d0726265ace66ae2d5db2cfeca
Sidharth Kushwaha <sid.kushwaha04@gmail.com>
2026-05-27 11:34:16 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+
+  compress: true,
+
+  poweredByHeader: false,
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
